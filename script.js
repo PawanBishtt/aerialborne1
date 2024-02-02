1b05fcb0ddd46bb01f5e680d1d7a291cac205b78
PawanBishtt <pawanbisht2001@gmail.com>
2024-02-02 11:30:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
+
+
+// swiper end
+
+
+
+// lenis
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+// lenis end
